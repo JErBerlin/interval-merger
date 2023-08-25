cb40c637c4f2719e903e41b8faa8967dff0766ba
JerBerlin <jeronir@hotmail.com>
2023-08-25 14:33:52 +0200

    improved generate tests script: better distributions and more options

--- generate_tests.sh
+++ generate_tests.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
+# TODO: not critical issues: 
+#       - check use or not of local variables across the script       
+#       - for high numbers and small sizes make incompatible parameters
+#       - for high numbers and high sizes number of intervals created less than expected. It produces EOF in logs.
+
 # Generate anchor points based on max_range and num_anchor_points
 generate_anchor_points() {
     local max_range=$1
-    local num_points=$2
+    local num_anchor_points=$2
 
-    local interval_distance=$(( max_range / (num_points - 1) ))
+    local distance=$(( max_range / (num_anchor_points - 1) ))
 
     local anchor_points=""
-    for (( i=0; i<num_points; i++ )); do
-        local anchor_point=$(( interval_distance * i ))
+    for (( i=0; i<num_anchor_points; i++ )); do
+        local anchor_point=$(( distance * i ))
         anchor_points+="$anchor_point "
     done
 
     echo "$anchor_points"
 }
 
-# Generate covering intervals based on the max_range, num_points and generated anchor points
+# Generate covering intervals based on the max_range, num_intervals and generated anchor points
 generate_covering_intervals() {
     local max_range=$1
-    local num_points=$2
-    local anchor_points_output=$3
+    local num_anchor_points=$2
+    local anchor_points_str=$3
 
-    # Convert the anchor_points_output to an array
-    local anchor_points=($anchor_points_output)
+    # Convert the anchor_points_str to an array
+    local anchor_points=($anchor_points_str)
 
     # Calculate the average distance between anchor points
-    local avg_distance=$(( max_range / (num_points - 1) ))
+    local avg_distance=$(( max_range / (num_anchor_points - 1) ))
 
     local intervals=""
     for anchor_point in "${anchor_points[@]}"; do
@@ -47,6 +52,18 @@ generate_covering_intervals() {
     echo "$intervals"
 }
 
+ensure_directories_exist() {
+    # Check if "./testdata/" exists
+    if [ ! -d "./testdata/" ]; then
+        mkdir -p "./testdata/"
+    fi
+
+    # Check if "./testdata/expected/" exists
+    if [ ! -d "./testdata/expected/" ]; then
+        mkdir -p "./testdata/expected/"
+    fi
+}
+
 generate_test_file() {
     local range=$1
     local size=$2
@@ -54,12 +71,15 @@ generate_test_file() {
     case $range in
         low) 
             max_range=100
+            num_anchor_points=$(( $max_range / 20 ))
             ;;
         mid)
             max_range=10000
+            num_anchor_points=$(( $max_range / 100 ))
             ;;
         high)
             max_range=2147483647  # Max of int32
+            num_anchor_points=$(( $max_range / 1000000 ))
             ;;
         *)
             echo "Invalid range!"
@@ -68,48 +88,62 @@ generate_test_file() {
 
     case $size in
         xs)
-            num_points=25
+            num_intervals=25
             ;;
         s)
-            num_points=125
+            num_intervals=125
             ;;
         m)
-            num_points=500
+            num_intervals=500
             ;;
         l)
-            num_points=12500
+            num_intervals=12500
             ;;
         xl)
-            num_points=125000
+            num_intervals=125000
+            ;;
+        # Warning: xxl can take few minutes to generate
+        xxl)
+            num_intervals=1000000
             ;;
         *)
             echo "Invalid size!"
             exit 1
     esac
 
-    # Set constants for anchor points and generating iterations
-    local num_anchor_points=5
-    local generating_iterations=5
+    # Number intervals effectively written should be approximately the required num_intervals 
+    generating_iterations=$(( $num_intervals / $num_anchor_points ))
 
+    # Ensure the directories exist, creating them if necessary
+    ensure_directories_exist
+    
     test_file_path="./testdata/$range.$size.txt"
     expected_file_path="./testdata/expected/$range.$size.txt"
 
-    > $test_file_path
-    > $expected_file_path
-
+    # Write the total number of intervals at the beginning of test and expected result files
+    # For the expected result file it `is always 1, by construction of the intervals
+    echo $num_intervals > $test_file_path
+    echo 1 > $expected_file_path
+    
     # Generate the anchor points
     local anchors=$(generate_anchor_points $max_range $num_anchor_points)
     
     # Generate the covering intervals and write to the test file
     for (( i=0; i<$generating_iterations; i++ )); do
-        local covering_intervals=$(generate_covering_intervals $max_range $num_points "$anchors")
+        local covering_intervals=$(generate_covering_intervals $max_range $num_anchor_points "$anchors")
         for interval in $covering_intervals; do
             echo "$interval" >> $test_file_path
         done
     done
 
-    echo "[$a,$b]" >> $expected_file_path
+    echo "[0,$max_range]" >> $expected_file_path
 }
+
+# Check arguments are present when calling generate_tests
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <range> <size>, where range in [low, mid, high] and size in [xs, s, m, l, xl, xxl]."
+    exit 1
+fi
 
 # Call the function
 generate_test_file $1 $2
